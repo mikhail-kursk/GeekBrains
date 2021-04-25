0800e6eef681e599d1d81228a8e49ce1edf9a9fa
Mikhail <mihail.kursk@mail.ru>
2021-04-25 17:40:01 +0300

    First Homework

---
+++ FirstHomework/FirstHomework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста, введите своё имя");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Привет, " + Name + ", сейчас " + DateTime.Now);
+
+        }
+    }
+}
