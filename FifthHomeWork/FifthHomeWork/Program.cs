511da6fcf175f60ceacf03b1c0f56b01d5d03c17
Mikhail Ivakhnenko <mihail.kursk@mail.ru>
2021-05-11 20:51:38 +0300

    Part1

---
+++ FifthHomeWork/FifthHomeWork/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+
+namespace FifthHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день, выберите номер тестового задания 1-4");
+            Console.WriteLine("Задание 1 - ввод с клавиатуры с сохранением в файл");
+            Console.WriteLine("Задание 2 - дописать текущее время в файл");
+            Console.WriteLine("Задание 3 - ввод с клавиатуры и сохранение в бинарный файл");
+            Console.WriteLine("Задание 4 - дерево каталогов с сохранением в файл");
+            Console.WriteLine("Задание 5 - дерево каталогов с сохранением в файл - рекурсия");
+            Console.WriteLine("Задание 6 - ToDo list");
+
+            string choice = Console.ReadLine();
+
+
+            switch (choice)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Выбрано задание 1 - ввод с клавиатуры с сохранением в файл" + "\n");
+
+                        Console.WriteLine("Введите данные для сохранения в файл text.txt");
+
+                        string str1 = Console.ReadLine();
+                        string filename = "text.txt";
+
+                        File.WriteAllText(filename, str1);
+
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Выбрано задание 2 - дописать текущее время в файл startup.txt" + "\n");
+
+                        string filename = "startup.txt";
+                        File.AppendAllText(filename, "\n" + Convert.ToString(DateTime.Now));
+
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Выбрано задание 3 - ввод с клавиатуры и сохранение в бинарный файл" + "\n");
+
+                        Console.WriteLine("Введите число от 0 до 255 для сохранения в файл binary.bin");
+
+                        string str1 = Console.ReadLine();
+                        byte[] byteArray = new byte[1] { 0 };
+
+                        try
+                        {
+                            byteArray[0] = Convert.ToByte(str1);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Произошла ошибка при конвертации данных");
+                            Console.WriteLine(e);
+                            break;
+                        }
+
+                        string filename = "binary.bin";
+
+                        File.WriteAllBytes(filename, byteArray);
+
+                        break;
+                    }
+
+                case "4":
+                    {
+                        Console.WriteLine("Выбрано задание 4 - дерево каталогов с сохранением в файл" + "\n");
+
+                        Console.WriteLine("Выберите путь");
+
+                        string str1 = Console.ReadLine();
+
+                        bool isExist = Directory.Exists(str1);
+                        
+                        if (!isExist)
+                        {
+                            Console.WriteLine("Выбранный путь не существует");
+                            break;
+                        }    
+
+
+
+
+                        break;
+                    }
+
+                case "6":
+                    {
+                        Console.WriteLine("Выбрано задание 5 - ToDo list" + "\n");
+
+                        Console.WriteLine("Выберите путь" + "\n");
+
+                        string path = Console.ReadLine();
+                        bool isExist = Directory.Exists(path);
+
+                        if (!isExist)
+                        {
+                            Console.WriteLine("Выбранный путь не существует");
+                            break;
+                        }
+
+                        Console.WriteLine("Переходим в каталог" + path + "\n");
+                        
+                        Console.WriteLine("Введите имя для списка задач");
+                        string name = Console.ReadLine();
+
+                        ToDoList list1 = new ToDoList(path + name);
+
+                        list1.NewTask("Задача 1");
+
+                        Console.WriteLine(list1.GetActiveTaskList());
+                        list1.ChangeTaskTitle(0, "Задача переименованна");
+                        Console.WriteLine(list1.GetActiveTaskList());
+                        list1.MarkTaskAsDone(0);
+
+                        Console.WriteLine("Список активных задач");
+                        Console.WriteLine(list1.GetActiveTaskList());
+
+                        Console.WriteLine("Список выполненных задач");
+                        Console.WriteLine(list1.GetDoneTaskList());
+
+
+
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Неверный ввод - введите число от 1 до 6");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ FifthHomeWork/FifthHomeWork/ToDo.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FifthHomeWork
+{
+
+    public class ToDoList
+    {
+
+        public string Name;
+        List<ToDo> Tasks = new List<ToDo> ();
+
+        public ToDoList(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+                Name = name;
+            else
+            {
+                Console.WriteLine("Неверное имя файла");
+            }
+        }
+
+
+        public bool ChangeToDoListName (string newName)
+        {
+            if (String.IsNullOrEmpty(newName) || !Directory.Exists(newName))
+            {
+                Console.WriteLine("Неверный путь");
+                return false;
+            }
+
+            Name = newName;
+            return true;
+        }
+
+        public (string, bool) GetTaskList ()
+        {
+            if (!String.IsNullOrEmpty(Name))
+                if(Tasks.Count > 0)
+                    foreach (ToDo task in Tasks)
+                    {
+                        Console.WriteLine(task.GetTaskNameAndStatus());
+                        return task.GetTaskNameAndStatus();
+                    }
+
+            return ( "" , false );
+
+        }
+        public string GetActiveTaskList ()
+        {
+            string result = "";
+
+            if (Tasks.Count > 0)
+                foreach (ToDo task in Tasks)
+                {
+                    var tempTask = task.GetTaskNameAndStatus();
+                    if (!tempTask.Item2)
+                        result += tempTask.Item1 + "     " + tempTask.Item2;
+                }
+
+            return result;
+        }
+        public string GetDoneTaskList ()
+        {
+            string result = "";
+
+            if (Tasks.Count > 0)
+                foreach (ToDo task in Tasks)
+                {
+                    var tempTask = task.GetTaskNameAndStatus();
+                    if (tempTask.Item2)
+                        result += tempTask.Item1 + "     " + tempTask.Item2;
+                }
+
+            return result;
+        }
+        public bool NewTask(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+                return false;
+
+            ToDo newTask = new ToDo(name);
+            Tasks.Add(newTask);
+
+            return true;
+        }
+
+        public bool ChangeTaskTitle(int number, string newTaskName)
+        {
+            bool result = Tasks[number].ChangeTitle(newTaskName);
+            if (!result)
+            {
+                Console.WriteLine("Не удалось переименовать задачу");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool MarkTaskAsDone(int number)
+        {
+            bool result = Tasks[number].MarkTaskAsDone();
+            if (!result)
+            {
+                Console.WriteLine("Не удалось изменить статус задачи");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+    }
+
+    public class ToDo
+    {
+        private string Title;
+        private bool IsDone = false;
+
+        public ToDo(string title)
+        {
+            Title = title;
+        }
+
+        public bool ChangeTitle (string newTitle)
+        {
+            if (!String.IsNullOrEmpty(newTitle))
+            {
+                Title = newTitle;
+                return true;
+            }
+
+            return false;
+        }
+
+        public (string, bool) GetTaskNameAndStatus()
+        {
+            return (Title, IsDone);
+        }
+
+        public string GetTaskNameAndStatusString()
+        {
+            return Title + "     " + IsDone;
+        }
+
+        public string GetTaskName()
+        {
+            return Title;
+        }
+
+        public bool MarkTaskAsDone()
+        {
+            IsDone = true;
+            return true;
+        }
+
+        public bool MarkTaskAsNotDone()
+        {
+            IsDone = false;
+            return true;
+        }
+    }
+}
