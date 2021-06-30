cee54d8a08127cd6874a2ff66f4e4d1ae32a2be9
Mikhail Ivakhnenko <mihail.kursk@mail.ru>
2021-06-30 15:20:33 +0300

    Initial commit

---
+++ HW_2_1/HW_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 " + isSimple(1));
+            Console.WriteLine("10 " + isSimple(10));
+            Console.WriteLine("7 " + isSimple(7));
+            Console.WriteLine("6 " + isSimple(6));
+            Console.WriteLine("144 " + isSimple(144));
+
+        }
+
+        static string isSimple(int n)
+        {
+            int d = 0;
+            int i = 2;
+
+
+            while (i < n)
+            {
+                if ((n % i) == 0)
+                    d++;
+                i++;
+            }
+
+            if (d == 0)
+                return "Простое";
+            else
+                return "Не простое";
+        }
+    }
+}
---
+++ HW_2_1/HW_2_1_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HW_2_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 число фиббоначи равно = " + FibbonaciRecursion(1, 1, 3, 10));
+            Console.WriteLine("5 число фиббоначи равно = " + FibbonaciRecursion(1, 1, 3, 5));
+            Console.WriteLine("20 число фиббоначи равно = " + FibbonaciRecursion(1, 1, 3, 20));
+            Console.WriteLine("100 число фиббоначи равно = " + FibbonaciRecursion(1, 1, 3, 500));
+
+
+            Console.WriteLine("10 число фиббоначи равно = " + FibbonaciCycle(1, 1, 3, 10));
+            Console.WriteLine("5 число фиббоначи равно = " + FibbonaciCycle(1, 1, 3, 5));
+            Console.WriteLine("20 число фиббоначи равно = " + FibbonaciCycle(1, 1, 3, 20));
+            Console.WriteLine("100 число фиббоначи равно = " + FibbonaciCycle(1, 1, 3, 500));
+        }
+
+        static double FibbonaciRecursion(double current, double prev, int number, int targetNumber)
+        {
+            double newValue = current + prev;
+
+            if (number == targetNumber)
+                return newValue;
+
+            return newValue = FibbonaciRecursion(newValue, current, number + 1, targetNumber);
+        }
+
+        static double FibbonaciCycle(double current, double prev, int number, int targetNumber)
+        {
+            double result = 0;
+
+            for (int i = number; i <= targetNumber; i++)
+            {
+                result = current + prev;
+                
+                prev = current;
+                current = result;
+            }
+
+            return result;
+        }
+    }
+}
